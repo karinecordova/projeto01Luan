daa91e132bb5ce6f39bffee17874c069ffdfc284
karinecordova <karinealessandracordova@gmail.com>
2019-09-25 15:11:31 -0300

    backupprojeto

---
+++ src/app/principaisbandas/principaisbandas.page.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-principaisbandas',
+  templateUrl: './principaisbandas.page.html',
+  styleUrls: ['./principaisbandas.page.scss'],
+})
+export class PrincipaisbandasPage implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
